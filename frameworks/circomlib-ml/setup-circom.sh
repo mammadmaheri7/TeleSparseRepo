6833f515a05db00ca31ee58e879fc820fa8c3618
mammadmaheri7 <mohamadmaheri@yahoo.com>
2024-10-16 21:24:20 +0100

    change version circomlib

--- frameworks/circomlib-ml/setup-circom.sh
+++ frameworks/circomlib-ml/setup-circom.sh
@@ -2,6 +2,9 @@ curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
 git clone https://github.com/iden3/circom.git
 
+# checkout to 2eaaa6dface934356972b34cab64b25d382e59de (version 2.1.9)
+git checkout 2eaaa6dface934356972b34cab64b25d382e59de
+
 cd circom
 
 cargo build --release
