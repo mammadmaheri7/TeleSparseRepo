65c8d868d17ceee48a205a2bad0ef19fbc2025bf
mammadmaheri7 <mohamadmaheri@yahoo.com>
2024-10-25 02:46:51 +0100

    add input to process files of circomlib

--- frameworks/circomlib-ml/benchmark.py
+++ frameworks/circomlib-ml/benchmark.py
@@ -699,8 +699,8 @@ def benchmark_cnn(test_images, predictions, layers, model_name, tmp_folder, inpu
         # add key "in" to final_json
         X = test_images[0]
         scalar = 18
-        h = 28
-        nChannels = 1
+        h = 32
+        nChannels = 3
         X_in = [[[int(X[i][j][0]*(10 ** scalar))] for j in range(h)] for i in range(h)]
         Input = [[[str(X_in[i][j][k] % p) for k in range(nChannels)] for j in range(h)] for i in range(h)]
         final_json["in"] = Input
