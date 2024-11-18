f0a64a1f572f7908371aed7fe027604ec97ea29e
mammadmaheri7 <mohamadmaheri@yahoo.com>
2024-11-18 23:02:56 +0000

    calculate acc on resnet20 (combination with nerual teleportaion)

--- frameworks/ezkl/benchmark.py
+++ frameworks/ezkl/benchmark.py
@@ -440,6 +440,7 @@ def benchmark_cnn(test_images, predictions, model, model_name, mode = "resources
             prefix_dir = "resnet20_teleport_ZO_temp_acc_only"
             # mkdir the directory
             os.makedirs(os.path.join(teleportation_code_address, prefix_dir), exist_ok=True)
+            os.makedirs(os.path.join(teleportation_code_address, prefix_dir, "images"), exist_ok=True)
             # step1: provide input data for the teleportation model in directory teleportation_data
             # store npy of the image in teleportation_code_address/prefix_dir/inputs
             np.save(os.path.join(teleportation_code_address, prefix_dir, "images", f"input_{i}.npy"), img.cpu().detach().numpy())
